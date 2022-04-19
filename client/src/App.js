e85d5468453f1da5c1dc4421da2379e4a16876aa
Lewis Heath <email@lewisheath.co.uk>
2022-04-19 18:02:27 +0100

    started on the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import InputCity from './components/InputCity';
+
 
 function App() {
+
+    const[data, setData] = useState({});
+
+    async function handleButtonClick(event, city){
+        event.preventDefault();
+        const response = await fetch(`http://localhost:5000/weather?city=${city}`);
+        const weather = await response.json();
+        console.log(weather);
+        setData(weather);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <InputCity setData={handleButtonClick} />
+        <div>
+            {/*if data is empty, show nothing, if data is not empty, show the data*/}
+            {Object.keys(data).length === 0 ? null : (
+                <div>
+                    <h1>Weather in {data.location.name}</h1>
+                    <p>Temperature: {data.current.temp_c} Celsius</p>
+                    <p>wind speed: {data.current.wind_mph} mph</p>
+                    <img src={data.current.condition.icon} alt="weather icon" />
+                </div>
+            )}
+        </div>
     </div>
   );
 }
---
+++ client/src/components/InputCity.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './InputCity.css'
+
+function InputCity({ setData }) {
+
+    const [city, setCity] = useState('');
+
+    const handleKeypress = e => {
+      //it triggers by pressing the enter key
+        if (e.keyCode === 13) {
+            setData(e, city);
+            setCity('');
+        }
+    };
+
+  return (
+    <div className='input-container'>
+        <div className="container">
+            <input type="text" placeholder="Enter city name" value={city} onChange={e => setCity(e.target.value)} onKeyDown={handleKeypress} className="form-control" />
+            {/* <button type="submit" onClick={e => setData(e, city)} className="btn btn-success btn-lg button">get weather</button> */}
+        </div>
+    </div>
+  )
+}
+
+export default InputCity
